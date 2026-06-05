ec97aae833b35af0ac1bb3e7b59bd86a2467ff93
Ivan Zapata <izapatapichinao@gmail.com>
2026-06-05 17:21:35 -0400

    Hito 3: renderización de componentes dinámicos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,15 @@ import CardPizza from "./CardPizza";
 import { pizzas } from "../assets/js/pizzas";
 
 export default function Home() {
-  const [arrayPizzas, setPizzas] = useState([pizzas]);
+  const [arrayPizzas, setPizzas] = useState(pizzas);
 
   return (
     <>
       <Header />
       <div className="row p-5">
-        {pizzas.map((pizza, index) => (
+        {pizzas.map((pizza) => (
           <CardPizza
-            key={index}
+            key={pizza.id}
             desc={pizza.desc}
             name={pizza.name}
             price={pizza.price}
